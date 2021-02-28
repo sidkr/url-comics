2964b5e130799c4cc52dceaf010a6d7218c179dc
Sid <siddhartha.kumar@live.com>
2021-02-28 13:45:51 -0500

    UX Improvements. Added auto share link generation

--- app.js
+++ app.js
@@ -1,20 +1,61 @@
-
-
 (function () {
-  window.onpopstate = history.onpushstate = function(e) {
-    fillText();
-   }
+  var urlParams = new URLSearchParams(window.location.search),
+    share = urlParams.get("share"),
+    editSection = document.getElementById("edit"),
+    generateBtn = document.getElementById("generate"),
+    shareSection = document.getElementsByClassName("share-container")[0],
+    shareLink = document.getElementById("share-link"),
+    toast = document.getElementsByClassName('toast')[0],
+    editMode = true, //default
 
-  var fillText = function(){
-    var delim = "_";
-    var hash = location.hash.substring(1)
-  
-    var inputArray = hash.split(delim);
-  
-    for(var i=1;i<inputArray.length; i++){
-      document.getElementById('p-'+i).innerHTML = decodeURI(inputArray[i]);
+   fillText = function (inputArray) {
+    if (!editMode) {
+      var delim = "_",
+        hash = location.hash.substring(1),
+        inputArray = hash.split(delim);
     }
+
+    for (var i = 0; i < 3; i++) {
+      document.getElementById("p-" + i).innerHTML = decodeURI(inputArray[i]);
+    }
+
+    document.getElementById("c").scrollIntoView();
   };
 
-  fillText()
+  if (share === "1") {
+    editMode = false;
+    editSection.style.display = "none";
+    fillText();
+  } else {
+    editSection.style.display = "block";
+    shareSection.style.display = "block";
+  }
+
+  generateBtn.addEventListener("click", function (e) {
+    var inputArray = [];
+    document.querySelectorAll("input").forEach(function (e) {
+      inputArray.push(decodeURI(e.value));
+    });
+    fillText(inputArray);
+    //Generate share link
+    var link =
+      window.location.href +
+      "?share=1#" +
+      inputArray[0] +
+      "_" +
+      inputArray[1] +
+      "_" +
+      inputArray[2];
+    shareLink.value = encodeURI(link);
+
+    //Copy to clipboard
+    shareLink.select();
+    document.execCommand("copy");
+
+    //Show toast
+    toast.className = "toast show";
+    setTimeout(function () {
+      toast.className = toast.className.replace("show", "");
+    }, 3000);
+  });
 })();
